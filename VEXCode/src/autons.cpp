2abb6928ccf507ad101ef6f26f83dc837b9082ef
Ak123yay <aarushkatam5@gmail.com>
2026-01-24 01:58:16 -0500

    small bug fixes

--- VEXCode/src/autons.cpp
+++ VEXCode/src/autons.cpp
@@ -77,10 +77,8 @@ void drive_example() {
 
   chassis.pid_drive_set(24_in, DRIVE_SPEED, true);
   chassis.pid_wait();
-
-
-  // chassis.pid_drive_set(-12_in, DRIVE_SPEED);
-  // chassis.pid_wait();
+  chassis.pid_drive_set(-12_in, DRIVE_SPEED);
+  chassis.pid_wait();
 
 
   // // amazonq-ignore-next-line
@@ -98,13 +96,14 @@ void turn_example() {
   // The first parameter is the target in degrees
   // The second parameter is max speed the robot will drive at
 
-
   chassis.pid_turn_set(90_deg, TURN_SPEED);
   chassis.pid_wait();
 
+  chassis.pid_turn_set(45_deg, TURN_SPEED);
+  chassis.pid_wait();
 
-
-
+  chassis.pid_turn_set(0_deg, TURN_SPEED);
+  chassis.pid_wait();
 }
 
 
